397b6ee6cb1f46da813fd1af81a86a8dff3648ba
HelenGachkovska <ogachkovska@gmail.com>
2023-06-20 10:11:49 +0300

    update

--- src/components/ContactsFilter/ContactFilter.jsx
+++ src/components/ContactsFilter/ContactFilter.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Input } from './styled';
 import { useDispatch, useSelector } from 'react-redux';
 import { filtred } from 'redux/contactSlice';
+import { stateSelectorFilters } from 'redux/selector';
 
 function ContactFilter() {
   const filter = useSelector(stateSelectorFilters);
